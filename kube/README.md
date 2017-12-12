90543ad40a5006a8cd386f806e04fa9b6d3e6d03
Rob Salmond <rob@salmond.ca>
2017-12-11 23:18:49 -0800

    kube fixups

--- kube/README.md
+++ kube/README.md
@@ -1,3 +1,9 @@
 ## requirements:
 
 vagrant plugin install vagrant-vbguest
+
+enable memory cgroups:
+ * add `"cgroup_enable=memory"` to `GRUB_CMDLINE_LINUX` in `/etc/default/grub`
+ * run `sudo update-grub2`
+ * `reboot -n`
+ 
--- kube/postscript.sh
+++ kube/postscript.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+#TODO: switch to Calico
 # enable full features of kube-router networking
 #kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter-all-features.yaml
 
+# enable calico
+kubectl apply -f manifests/calico-with-portmap.yaml
+
 # allow pods to be scheduled on the master
-#kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl taint nodes --all node-role.kubernetes.io/master-
 
 # create secret for pulling containers from gcr
-echo "$(cat ~/.ssh/rsa-servers-bdeedbfb9414.json)"
 kubectl create secret docker-registry gcr-json-key --docker-server https://gcr.io --docker-username _json_key --docker-email=containers@rsa-servers.iam.gserviceaccount.com --docker-password="$(cat ~/.ssh/rsa-servers-bdeedbfb9414.json)"
 
 # give gcr secret to the default k8s service account
-#kubectl patch serviceaccount default -p "{\"imagePullSecrets\": [{\"name\": \"gcr-json-key\"}]}"
+kubectl patch serviceaccount default -p "{\"imagePullSecrets\": [{\"name\": \"gcr-json-key\"}]}"
