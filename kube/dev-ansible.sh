5e1a65e5da311ea6f3811b4bb271f876179f01fc
Rob Salmond <rob@salmond.ca>
2018-02-07 21:44:39 -0800

    use python3

--- kube/dev-ansible.sh
+++ kube/dev-ansible.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # run an ansible playbook against local vagrant machine
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -vvvv -u vagrant --private-key .vagrant/machines/default/virtualbox/private_key $@
+#ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -vvvv -u vagrant --private-key .vagrant/machines/default/virtualbox/private_key $@
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u vagrant --private-key .vagrant/machines/default/virtualbox/private_key -e 'ansible_python_interpreter=/usr/bin/python3' $@
