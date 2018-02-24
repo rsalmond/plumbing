3dbc9e016f71ec8344476266f1cab93f546b15fc
Rob Salmond <rob@salmond.ca>
2018-02-24 13:13:44 -0800

    install nginx ingress with helm instead

--- kube/postscript.sh
+++ kube/postscript.sh
@@ -6,16 +6,10 @@ kubectl create secret docker-registry gcr-json-key --docker-server https://gcr.i
 # give gcr secret to the default k8s service account
 kubectl patch serviceaccount default -p "{\"imagePullSecrets\": [{\"name\": \"gcr-json-key\"}]}"
 
-# install nginx ingress controller https://github.com/kubernetes/ingress-nginx/blob/master/deploy/README.md
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/default-backend.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/configmap.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/tcp-services-configmap.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/udp-services-configmap.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/rbac.yaml
-
-# install modified nginx pod with hostnetworking enabled (so we can listen on 80/443)
-kubectl apply -f manifests/nginx-deployment-with-rbac.yaml
-
 # give svc account cluster-admin so helm will work
 kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:default
+
+helm init
+
+# install nginx ingress controller
+helm install stable/nginx-ingress --name my-nginx --set rbac.create=true --set controller.hostNetwork=true
