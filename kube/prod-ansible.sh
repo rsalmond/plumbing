ae366cd4cdeae6b78acd4575336a7d340b5f7044
Rob Salmond <rob@salmond.ca>
2018-02-24 14:08:00 -0800

    passwords are cool

--- kube/prod-ansible.sh
+++ kube/prod-ansible.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # run ansible playbook against brazen host
-ansible-playbook -i "168.235.108.115," -u phro --private-key ~/.ssh/id_brazen $@ 
+ansible-playbook -i "168.235.108.115," -u phro --private-key ~/.ssh/id_brazen --ask-become-pass $@
