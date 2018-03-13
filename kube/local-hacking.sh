b73fa4c4ac639fc3659678e75d7d03671b93bb63
Rob Salmond <rob@salmond.ca>
2018-03-12 22:05:38 -0700

    bunch of clean up, add helm install of openvpn and nginx ingress for web and vpn traffic

--- kube/local-hacking.sh
+++ kube/local-hacking.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-set -e
-
 # start and then reboot the vagrant vm (to make the grub config change take hold)
-vagrant up
+vagrant up && sleep 3
 
-sleep 2
 ./dev-ansible.sh ansible/cgroup.yaml
 
 # need to reboot after changing cgroups
-vagrant reload
+vagrant reload && sleep 3
 
-sleep 2
 # install all the requirements, repos, etc
-./dev-ansible.sh ansible/kubernetes-base.yaml
+./dev-ansible.sh ansible/base.yaml
 
 # initialize kubernetes and download kubeconfig file
 ./dev-ansible.sh ansible/kube-init.yaml
@@ -22,10 +18,12 @@ sleep 2
 cp ansible/kubeconfig/kubernetes/etc/kubernetes/admin.conf ~/.kube/config
 sed -i 's/10\.0\.2\.15/kubernetes/' ~/.kube/config
 
-# enable full features of kube-router networking
+# enable kube router with firewall and proxy
 #kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter-all-features.yaml
+# enable kube router with firewall
 #kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
-kubectl apply -f manifests/kubeadm-kuberouter.yaml
+# enable kube router with no proxy and no firewall
+kubectl apply -f manifests/00-kubeadm-kuberouter.yaml
 
 # allow pods to be scheduled on the master
 kubectl taint nodes --all node-role.kubernetes.io/master-
--- kube/postscript.sh
+++ kube/postscript.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+# init steps for once the cluster is up and running
 
-# create secret for pulling containers from gcr
+# create secret for pulling containers from private gcr registry
 kubectl create secret docker-registry gcr-json-key --docker-server https://gcr.io --docker-username _json_key --docker-email=containers@rsa-servers.iam.gserviceaccount.com --docker-password="$(cat ~/.ssh/rsa-servers-bdeedbfb9414.json)"
 
 # give gcr secret to the default k8s service account
 kubectl patch serviceaccount default -p "{\"imagePullSecrets\": [{\"name\": \"gcr-json-key\"}]}"
 
-# give svc account cluster-admin so helm will work
-kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:default
+# create a svc account and rolebinding for tiller and initialize helm
+kubectl apply -f manifests/01-tiller-role.yaml
+helm init --service-account tiller
 
-helm init
+# create a persistent volume claim (for keys) for openvpn and initialize it
+kubectl apply -f manifests/02-openvpn-persistent-vol.yaml
+helm install stable/openvpn --version 2.0.2 --set persistence.existingClaim=openvpn --set service.type=NodePort --set service.externalPort=8080
 
-# install nginx ingress controller
-helm install stable/nginx-ingress --name my-nginx --set rbac.create=true --set controller.hostNetwork=true
+# install nginx ingress controller (including TCP pass through for VPN connectivity)
+helm install stable/nginx-ingress --set rbac.create=true --set controller.service.type=NodePort --set controller.hostNetwork=true --set tcp.8080=default/`kubectl get deployment -l 'chart in (openvpn-2.0.2)' -o jsonpath="{.items[0].metadata.name}"`:8080
