d83af1aab3b4298a270426c7c1dda462861109b3
Rob Salmond <rob@salmond.ca>
2019-02-25 19:17:23 -0700

    nuke another one

--- terraform/graildiary.net/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "plumbing-tf-state"
-    key     = "graildiary.net/terraform.tfstate"
-    region  = "us-east-1"
-    profile = "personal"
-  }
-}
--- terraform/graildiary.net/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "vps_ip" {
-  description = "IP of my VPS (brazen)"
-  type        = "string"
-  default     = "168.235.108.115"
-}
