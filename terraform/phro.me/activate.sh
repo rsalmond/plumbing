28e67ac050772fefc6e528355194dab6ada604ea
Rob Salmond <rob@salmond.ca>
2019-02-25 19:14:15 -0700

    nuke old domains

--- terraform/phro.me/activate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ACCOUNT=personal
-ENVIRON=phro.me
-
-terraform remote config -backend=s3 -backend-config="bucket=plumbing-tf-state" -backend-config="key=$ENVIRON/terraform.tfstate" -backend-config="region=us-east-1" -backend-config="profile=$ACCOUNT"
-
-echo "AWS account: $ACCOUNT, active terraform environment: $ENVIRON"
--- terraform/phro.me/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "vps_ip" {
-  description = "IP of my VPS"
-  type        = "string"
-  default     = "107.161.27.162"
-}
