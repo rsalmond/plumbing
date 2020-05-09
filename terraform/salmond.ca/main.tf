851449b9ee9ff929c071e653c6cf0dbcec768ab8
Rob Salmond <rob@salmond.ca>
2020-05-09 12:47:36 -0600

    box is deprecated

--- terraform/salmond.ca/main.tf
+++ terraform/salmond.ca/main.tf
@@ -18,17 +18,6 @@ resource "aws_route53_record" "rob_salmond_ca" {
   ]
 }
 
-resource "aws_route53_record" "box_salmond_ca" {
-  zone_id = aws_route53_zone.salmond_ca.zone_id
-  name    = "box.${aws_route53_zone.salmond_ca.name}"
-  type    = "A"
-  ttl     = "300"
-
-  records = [
-    var.vps_ip,
-  ]
-}
-
 resource "aws_route53_record" "keybase" {
   zone_id = aws_route53_zone.salmond_ca.zone_id
   name    = aws_route53_zone.salmond_ca.name
@@ -50,16 +39,5 @@ resource "aws_route53_record" "mx" {
     "10 ALT2.ASPMX.L.GOOGLE.COM",
     "10 ASPMX.L.GOOGLE.COM",
     "10 ALT1.ASPMX.L.GOOGLE.COM",
-  ]
-}
-
-resource "aws_route53_record" "test_salmond_ca" {
-  zone_id = aws_route53_zone.salmond_ca.zone_id
-  name    = "test.${aws_route53_zone.salmond_ca.name}"
-  type    = "A"
-  ttl     = "300"
-
-  records = [
-    var.brazen_ip
   ]
 }
--- terraform/salmond.ca/variables.tf
+++ terraform/salmond.ca/variables.tf
@@ -1,9 +1,3 @@
-variable "vps_ip" {
-  description = "IP of my VPS"
-  type        = string
-  default     = "107.161.27.162"
-}
-
 variable "brazen_ip" {
   description = "IP of my VPS"
   type        = string
