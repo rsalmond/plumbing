5ba41dffc3a301452de69cf0a3de9034ae124b4c
rob salmond <rob@salmond.ca>
2024-03-25 12:10:39 -0400

    add sashas domain

---
+++ terraform/technicolorwizard.com/main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = "personal"
+}
+
+resource "aws_route53_zone" "technicolorwizard" {
+  comment = "HostedZone created by Route53 Registrar"
+  name = var.domain
+}
+
+resource "aws_route53_record" "mx" {
+  zone_id = aws_route53_zone.technicolorwizard.zone_id
+  name    = aws_route53_zone.technicolorwizard.name
+  type    = "MX"
+  ttl     = "300"
+
+  records = [
+    "10 mx1.improvmx.com",
+    "20 mx2.improvmx.com",
+  ]
+}
+
+resource "aws_route53_record" "txt" {
+  zone_id = aws_route53_zone.technicolorwizard.zone_id
+  name    = aws_route53_zone.technicolorwizard.name
+  type    = "TXT"
+  ttl     = "300"
+
+  records = [
+    "v=spf1 include:spf.improvmx.com ~all",
+  ]
+}
+
+resource "aws_route53_record" "pizza_a" {
+  zone_id = aws_route53_zone.technicolorwizard.zone_id
+  name    = aws_route53_zone.technicolorwizard.name
+  type    = "A"
+  ttl     = "300"
+
+  records = [
+    "89.106.200.1" // redirect.pizza
+  ]
+}
+
+resource "aws_route53_record" "pizza_cname" {
+  zone_id = aws_route53_zone.technicolorwizard.zone_id
+  name    = "www.${aws_route53_zone.technicolorwizard.name}"
+  type    = "CNAME"
+  ttl     = "300"
+
+  records = [
+    "edge.redirect.pizza" // redirect.pizza
+  ]
+}
