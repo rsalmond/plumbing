0f2cb36318f3fd1ad83c7e12d305e1ee6994559f
rob salmond <rob@salmond.ca>
2024-10-22 11:58:59 -0400

    istio fixes

--- kube/postscript.sh
+++ kube/postscript.sh
@@ -4,13 +4,13 @@
 set -eu
 
 # create secret for pulling containers from private gcr registry
-kubectl delete secret gcr-json-key
+kubectl delete secret gcr-json-key || true
 kubectl create secret docker-registry gcr-json-key --docker-server https://gcr.io --docker-username _json_key --docker-email=containers@rsa-servers.iam.gserviceaccount.com --docker-password="$(cat ~/.ssh/rsa-servers-bdeedbfb9414.json)"
 
 # give gcr secret to the default k8s service account
 kubectl patch serviceaccount default -p "{\"imagePullSecrets\": [{\"name\": \"gcr-json-key\"}]}"
 
-istioctl install --skip-confirmation -f manifestsv3/istio-1.20-modified-profile.yaml
+./istioctl install --skip-confirmation -f manifestsv3/istio-1.20-modified-profile.yaml
 
 helm install certman -f helm-values/cert-manager-prod.yaml jetstack/cert-manager
 kubectl apply -f manifestsv3/cert-issuer-prod.yaml
