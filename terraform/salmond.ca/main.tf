8f1a0d0e724a26584e17c4d41169ca5954150426
rob salmond <rob@salmond.ca>
2025-01-26 16:51:54 -0500

    add sendgrid

--- terraform/salmond.ca/main.tf
+++ terraform/salmond.ca/main.tf
@@ -26,28 +26,6 @@ resource "aws_route53_record" "rob_salmond_ca" {
   ]
 }
 
-resource "aws_route53_record" "svcb" {
-  zone_id = aws_route53_zone.salmond_ca.zone_id
-  name    = "rob.${aws_route53_zone.salmond_ca.name}"
-  type    = "SVCB"
-  ttl     = "300"
-
-  records = [
-    "1 rob.salmond.ca alpn=h2"
-  ]
-}
-
-resource "aws_route53_record" "https" {
-  zone_id = aws_route53_zone.salmond_ca.zone_id
-  name    = "rob.${aws_route53_zone.salmond_ca.name}"
-  type    = "HTTPS"
-  ttl     = "300"
-
-  records = [
-    "1 rob.salmond.ca alpn=h2"
-  ]
-}
-
 resource "aws_route53_record" "keybase" {
   zone_id = aws_route53_zone.salmond_ca.zone_id
   name    = aws_route53_zone.salmond_ca.name
@@ -59,6 +37,51 @@ resource "aws_route53_record" "keybase" {
     "v=spf1 include:_spf.google.com include:fearisthevibekiller.com ~all"
   ]
 }
+
+/*************** sendgrid *******************/
+
+resource "aws_route53_record" "sendgrid0" {
+  zone_id = aws_route53_zone.salmond_ca.zone_id
+  name = "em6779.${aws_route53_zone.salmond_ca.name}"
+  type = "CNAME"
+  ttl = "300"
+  records = [
+    "u50069060.wl061.sendgrid.net"
+  ]
+}
+
+resource "aws_route53_record" "sendgrid1" {
+  zone_id = aws_route53_zone.salmond_ca.zone_id
+  name = "s1._domainkey.${aws_route53_zone.salmond_ca.name}"
+  type = "CNAME"
+  ttl = "300"
+  records = [
+    "s1.domainkey.u50069060.wl061.sendgrid.net"
+  ]
+}
+
+resource "aws_route53_record" "sendgrid2" {
+  zone_id = aws_route53_zone.salmond_ca.zone_id
+  name = "s2._domainkey.${aws_route53_zone.salmond_ca.name}"
+  type = "CNAME"
+  ttl = "300"
+  records = [
+    "s2.domainkey.u50069060.wl061.sendgrid.net"
+  ]
+}
+
+resource "aws_route53_record" "sendgrid_dmarc" {
+  zone_id = aws_route53_zone.salmond_ca.zone_id
+  name = "_dmarc.${aws_route53_zone.salmond_ca.name}"
+  type = "TXT"
+  ttl = "300"
+  records = [
+    "v=DMARC1; p=none;"
+  ]
+}
+
+
+/*************** gmail *******************/
 
 resource "aws_route53_record" "mx" {
   zone_id = aws_route53_zone.salmond_ca.zone_id
@@ -72,6 +95,8 @@ resource "aws_route53_record" "mx" {
     "10 ALT1.ASPMX.L.GOOGLE.COM",
   ]
 }
+
+/*************** terry *******************/
 
 resource "aws_route53_record" "terry" {
   zone_id = aws_route53_zone.salmond_ca.zone_id
